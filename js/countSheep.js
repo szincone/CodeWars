5fd4fc739703880c67703db2a47a0bf713293811
szincone <szincone.work@gmail.com>
2018-07-07 00:59:25 -0600

    countSheep.js

---
+++ js/countSheep.js
@@ -0,0 +1,9 @@
+// returns a string counting sheep, based off your param
+var countSheep = function (num){
+  //your code here
+  let sheepArr = [];
+  for (let i = 0; i < num; i++) {
+    sheepArr.push((i+1) + " sheep...");
+  }
+  return sheepArr.join("");
+}
