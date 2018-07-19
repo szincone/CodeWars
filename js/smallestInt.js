07a7aa5722a2799d25e4fd5d08b5ee1fcf984557
szincone <szincone.work@gmail.com>
2018-07-18 22:30:25 -0600

    Create smallestInt.js

---
+++ js/smallestInt.js
@@ -0,0 +1,7 @@
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    // Math.min return lowest valued number passed into it
+    // spread operator to get all args
+    return Math.min(...args);
+  }
+}
