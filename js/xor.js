f582213f69ee0cc0092b003a62a27c54c501b98b
szincone <szincone.work@gmail.com>
2018-07-19 20:52:15 -0600

    Create xor.js

---
+++ js/xor.js
@@ -0,0 +1,12 @@
+function xor(a, b) {
+  // TODO: Program Me
+  if (a && b === !false) {
+    return false;
+  }
+  else if (a || b === true){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
