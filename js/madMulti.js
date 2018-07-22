367abff8dc2c960f0c7d213c831eea07bfb32aba
szincone <szincone.work@gmail.com>
2018-07-22 00:47:59 -0600

    Shortened, made multiplication more consistent

--- js/madMulti.js
+++ js/madMulti.js
@@ -1,2 +1,3 @@
-// divide by reciprocal, bitwise OR to get rid of decimals
-const madMul=(a,b)=>a/(1/b)|0// had to be under 30 chars
+// divide by reciprocal, bitwise gets rid of decimals,
+// .5 makes the multiplication consistent (had to be under 30 chars)
+madMul=(a,b)=>~~(a/(1/b)+.5)
