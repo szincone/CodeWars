deab5fd01fb560f78174e4ef2114101faadffc79
szincone <szincone.work@gmail.com>
2018-12-28 13:08:54 -0700

    create getCount.py

---
+++ python/getCount.py
@@ -0,0 +1,8 @@
+def getCount(inputStr):
+    num_vowels = 0
+    for x in inputStr:
+        if (x == "a") or (x == "e") or (x == "i") or (x == "o") or (x == "u"):
+            num_vowels += 1
+        else:
+            num_vowels += 0
+    return num_vowels
